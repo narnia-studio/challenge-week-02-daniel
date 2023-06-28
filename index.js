bd8a163f9b034b29517ba221959cd8584703c5d2
Family C <danielbarr16@gmail.com>
2023-06-28 10:18:18 -0300

    feat: cleanup unnecessary conditional

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ function repeatedGroups(numbersArray) {
     let arrIndex = 0;
 
     numbersArray.forEach((num) => {
-        if (!groups[arrIndex]) {
-            groups[arrIndex] = [];
-        }
-
         if (num !== previous) {
             arrIndex = arrIndex + 1;
             groups[arrIndex] = [num];
@@ -29,5 +25,5 @@ function repeatedGroups(numbersArray) {
 console.log(repeatedGroups([1, 2, 2, 4, 5]));
 // return [[2, 2]]
 
-// console.log(repeatedGroups([1, 1, 0, 0, 8, 4, 4, 4, 3, 2, 1, 9, 9]));
+console.log(repeatedGroups([1, 1, 0, 0, 8, 4, 4, 4, 3, 2, 1, 9, 9]));
 // return [[1, 1], [0, 0], [4, 4, 4], [9, 9]]
